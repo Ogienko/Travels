16654afeb574249ea33d9555c877d91a2b6f7978
Evgeniy Ogienko <mister.ogienko@gmail.com>
2017-08-17 03:09:57 +0300

    Added model user & context

---
+++ Travels.Common/DAL/User.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Travels.Common.DAL {
+
+    public class User {
+
+        public int Id { get; set; }
+
+        public string Email { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Gender { get; set; }
+
+        public DateTime BirthDate { get; set; }
+    }
+}
