960c3ff5cf5ca6477a027a4bb55cc54f9b062fd7
Evgeniy Ogienko <mister.ogienko@gmail.com>
2017-08-18 20:18:36 +0300

    Added /users/<id>/visits

--- Travels/Controllers/UsersController.cs
+++ Travels/Controllers/UsersController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using Travels.Common;
 using Travels.Common.Models;
 using Travels.Common.Models.BaseModels;
+using Travels.Models;
 
 namespace Travels.Controllers {
 
@@ -29,6 +30,48 @@ namespace Travels.Controllers {
             return new JsonResult(user);
         }
 
+        [HttpGet("{id}/visits")]
+        public IActionResult GetUserVisits(int id, [FromQuery]GetUserVisitsParameters parameters) {
+
+            var user = _context.Users.FirstOrDefault(_ => _.Id == id);
+
+            if (user == null) {
+                return NotFound();
+            }
+
+            var query = _context.Visits.Where(_ => _.UserId == id).Join(_context.Locations, visit => visit.LocationId, location => location.Id, (visit, location) => new {
+                VisitedAt = visit.VisitedAt,
+                Mark = visit.Mark,
+                Place = location.Place,
+                Country = location.Country,
+                Distance = location.Distance
+            });
+
+            if (parameters.FromDate.HasValue) {
+                query = query.Where(_ => _.VisitedAt > parameters.FromDate);
+            }
+
+            if (parameters.ToDate.HasValue) {
+                query = query.Where(_ => _.VisitedAt < parameters.ToDate);
+            }
+
+            if (!String.IsNullOrEmpty(parameters.Country)) {
+                query = query.Where(_ => _.Country == parameters.Country);
+            }
+
+            if (parameters.ToDistance.HasValue) {
+                query = query.Where(_ => _.Distance < parameters.ToDistance);
+            }
+
+            var result = query.Select(_ => new {
+                Mark = _.Mark,
+                VisitedAt = _.VisitedAt,
+                Place = _.Place
+            }).OrderBy(_ => _.VisitedAt).ToList();
+
+            return new JsonResult(result);
+        }
+
         [HttpPost("{id}")]
         public IActionResult Post(int id, [FromBody]UserBase user) {
 
@@ -37,7 +80,7 @@ namespace Travels.Controllers {
             }
 
             var _user = _context.Users.FirstOrDefault(_ => _.Id == id);
-             
+
             if (_user == null) {
                 return NotFound();
             }
---
+++ Travels/Models/GetUserVisitsParameters.cs
@@ -0,0 +1,13 @@
+﻿namespace Travels.Models {
+
+    public class GetUserVisitsParameters {
+
+        public int? FromDate { get; set; }
+
+        public int? ToDate { get; set; }
+
+        public string Country { get; set; }
+
+        public int? ToDistance { get; set; }
+    }
+}
