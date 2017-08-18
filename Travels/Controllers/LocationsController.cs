344ab711b4151332f9f55be4b4ff9c4b8439ad91
Evgeniy Ogienko <mister.ogienko@gmail.com>
2017-08-18 21:43:10 +0300

    Added /locations/<id>/avg

--- Travels/Controllers/LocationsController.cs
+++ Travels/Controllers/LocationsController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using Travels.Common;
 using Travels.Common.Models;
 using Travels.Common.Models.BaseModels;
+using Travels.Models;
 
 namespace Travels.Controllers {
 
@@ -27,6 +28,51 @@ namespace Travels.Controllers {
             }
 
             return new JsonResult(location);
+        }
+
+        [HttpGet("{id}/avg")]
+        public IActionResult GetLocationsAvg(int id, [FromQuery]GetLocationsAvgParameters parameters) {
+
+            var location = _context.Locations.FirstOrDefault(_ => _.Id == id);
+
+            if (location == null) {
+                return NotFound();
+            }
+
+            var query = _context.Visits.Where(_ => _.LocationId == id).Join(_context.Users, visit => visit.UserId, user => user.Id, (visit, user) => new {
+                VisitedAt = visit.VisitedAt,
+                Mark = visit.Mark,
+                Gender = user.Gender,
+                BirthDate = user.BirthDate
+            });
+
+            if (parameters.FromDate.HasValue) {
+                query = query.Where(_ => _.VisitedAt > parameters.FromDate);
+            }
+
+            if (parameters.ToDate.HasValue) {
+                query = query.Where(_ => _.VisitedAt < parameters.ToDate);
+            }
+
+            var utcNow = DateTimeOffset.UtcNow.ToUnixTimeSeconds();
+
+            if (parameters.FromAge.HasValue) {
+                query = query.Where(_ => utcNow - _.BirthDate.Value > parameters.FromAge);
+            }
+
+            if (parameters.ToAge.HasValue) {
+                query = query.Where(_ => utcNow - _.BirthDate.Value < parameters.ToAge);
+            }
+
+            if (!String.IsNullOrEmpty(parameters.Gender)) {
+                query = query.Where(_ => _.Gender == parameters.Gender);
+            }
+
+            var avg = query.Average(_ => _.Mark);
+
+            var result = avg.HasValue ? avg.Value.ToString("#.#####") : "0";
+
+            return new JsonResult(result);
         }
 
         [HttpPost("{id}")]
---
+++ Travels/Models/GetLocationsAvgParameters.cs
@@ -0,0 +1,15 @@
+﻿namespace Travels.Models {
+
+    public class GetLocationsAvgParameters {
+
+        public int? FromDate { get; set; }
+
+        public int? ToDate { get; set; }
+
+        public int? FromAge { get; set; }
+
+        public int? ToAge { get; set; }
+
+        public string Gender { get; set; }
+    }
+}
